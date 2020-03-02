9f713aec4ac7bc252371c54127351407108856be
Amir A. Shabani <amirshabani011@gmail.com>
2020-03-02 21:23:08 +0330

    Better addresses for executing create.sh

--- bain.sh
+++ bain.sh
@@ -2,6 +2,7 @@
 
 file=$1
 last_changed=`< /sys/class/power_supply/BAT0/capacity`
+$HOME/code/bain/create.sh $file $last_changed
 
 while true
 do
@@ -9,7 +10,7 @@ do
 
 	if [[ "$last_changed" != "$current" ]]
 	then
-		./create.sh $file $current
+		$HOME/code/bain/create.sh $file $current
 		last_changed=$current
 	fi
 	sleep 5
