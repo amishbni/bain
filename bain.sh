f704ed26be4099b1cf7601354de0f10e7cfdf095
Amir A. Shabani <amirshabani011@gmail.com>
2020-03-03 15:48:45 +0330

    Add create_and_set function to remove the need for another bash file

--- bain.sh
+++ bain.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+create_and_set() {
+	orig=$1
+	perc=$2
+	size=`identify -format '%wx%h' $orig`
+
+	if [ "$perc" -ge 30 ]; then
+		color='#5BC236'
+	else
+		color='#BF131C'
+	fi
+	convert $orig -gravity South -crop x$perc% -fuzz 50% -fill $color -opaque '#8FBCBB' -background transparent -extent $size out.png
+
+	convert $orig out.png -gravity Center -composite -background '#2E3440' -gravity Center -extent 3840x2160 background.png
+
+	feh --bg-scale background.png
+}
+
 file=$1
 last_changed=`< /sys/class/power_supply/BAT0/capacity`
-$HOME/bain/create.sh $file $last_changed
+create_and_set $file $last_changed
 
 while true
 do
@@ -10,7 +27,7 @@ do
 
 	if [[ "$last_changed" != "$current" ]]
 	then
-		$HOME/bain/create.sh $file $current
+		create_and_set $file $current
 		last_changed=$current
 	fi
 	sleep 5
