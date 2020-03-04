cf69675a63261580262a8f851ca3cdb7818e7eae
Casarin Filippo <casarin.filippo17@gmail.com>
2020-03-04 12:31:45 +0100

    Added refresh speed flag.

--- bain.sh
+++ bain.sh
@@ -43,11 +43,13 @@ Usage: ./bain.sh [-h] [-b BATTERY] [-c COLOR_CHARGE] [-u COLOR_UNCHARGE] IMAGE
   -b, --battery BATTERY             the battery of the computer, if not set the programm will try to find it automatically
   -c, --charge COLOR_CHARGE         color for charge battery, defualt #5BC236
   -u, --uncharge COLOR_UNCHARGE     color for uncharge battery, defualt #BF131C
+  -r, --refresh                     refresh speed, defualt 5 seconds
   IMAGE                             the image to use for the background
 EOF
 		exit
 }
 
+REFRESH=5
 COLOR_CHARGE='#5BC236'
 COLOR_UNCHARGE='#BF131C'
 
@@ -57,6 +59,7 @@ while [ "$#" -gt 0 ]; do
 		-b|--battery) battery_path="$2"; shift 2;;
 		-c|--charge) COLOR_CHARGE="$2"; shift 2;;
 		-u|--uncharge) COLOR_UNCHARGE="$2"; shift 2;;
+		-r|--refresh) REFRESH="$2"; shift 2;;
 		*) file="$HOME/bain/images/$1.png"; shift 1;;
 	esac
 done
@@ -75,5 +78,5 @@ do
 		create_and_set $file $current
 		last_changed=$current
 	fi
-	sleep 5
+	sleep $REFRESH
 done
