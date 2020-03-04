5b7e3690330b947409fc7659fd1a095d81ff7a5f
Amir A. Shabani <amirshabani011@gmail.com>
2020-03-04 14:58:00 +0330

    Fix typo and change variable names
    On line 59, the status of battery is referred to as $status, which is a
    reserved variable. This commit fixes that.
    Also, variable names needed changing to be more descriptive. This commit
    changes them.

--- bain.sh
+++ bain.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 create_and_set() {
-	orig=$1
-	perc=$2
-	status=$3
-	size=`identify -format '%wx%h' $orig`
+	original_image=$1
+	battery_percentage=$2
+	battery_status=$3
+	image_size=`identify -format '%wx%h' $original_image`
 
-	if [[ "$status" == "Charging" ]]; then
+	if [[ "$battery_status" == "Charging" ]]; then
 		color='#FFFF00'
 	else
-		if [ "$perc" -ge 30 ]; then
+		if [ "$battery_percentage" -ge 30 ]; then
 			color='#5BC236'
 		else
 			color='#BF131C'
 		fi
 	fi
-	convert $orig -gravity South -crop x$perc% -fuzz 50% -fill $color -opaque '#8FBCBB' -background transparent -extent $size out.png
+	convert $original_image -gravity South -crop x$battery_percentage% -fuzz 50% -fill $color -opaque '#8FBCBB' -background transparent -extent $image_size out.png
 
-	convert $orig out.png -gravity Center -composite -background '#2E3440' -gravity Center -extent 3840x2160 background.png
+	convert $original_image out.png -gravity Center -composite -background '#2E3440' -gravity Center -extent 3840x2160 background.png
 
 	feh --no-fehbg --bg-scale background.png
 }
@@ -42,21 +42,21 @@ find_battery_path() {
 
 file="$HOME/bain/images/$1.png"
 battery_path=$(find_battery_path)
-last_changed=`< $battery_path/capacity`
+last_capacity=`< $battery_path/capacity`
 last_status=`< $battery_path/status`
-create_and_set $file $last_changed $last_status
+create_and_set $file $last_capacity $last_status
 
 while true
 do
-	current=`< $battery_path/capacity`
-	_status=`< $battery_path/status` # can not use $status because it is reserved
+	current_capacity=`< $battery_path/capacity`
+	current_status=`< $battery_path/status`
 
-	if [[ "$last_changed" != "$current" ]] || [[ "$_status" != "$last_status" ]]
+	if [[ "$current_capacity" != "$last_capacity" ]] || [[ "$current_status" != "$last_status" ]]
 	then
-		echo $current
-		create_and_set $file $current $_status
-		last_changed=$current
-		last_status=$status
+		echo $current_capacity
+		create_and_set $file $current_capacity $current_status
+		last_capacity=$current_capacity
+		last_status=$current_status
 	fi
 	sleep 5
 done
