ab9800e33868a8ef9e38897303236fbc5bd8f632
Casarin Filippo <casarin.filippo17@gmail.com>
2020-03-04 10:14:43 +0100

    Added command line arguments.

--- bain.sh
+++ bain.sh
@@ -6,9 +6,9 @@ create_and_set() {
 	size=`identify -format '%wx%h' $orig`
 
 	if [ "$perc" -ge 30 ]; then
-		color='#5BC236'
+		color=$COLOR_CHARGE
 	else
-		color='#BF131C'
+		color=$COLOR_UNCHARGE
 	fi
 	convert $orig -gravity South -crop x$perc% -fuzz 50% -fill $color -opaque '#8FBCBB' -background transparent -extent $size out.png
 
@@ -35,8 +35,34 @@ find_battery_path() {
 
 }
 
-file=$1
-battery_path=$(find_battery_path)
+cat_help() {
+		cat <<EOF
+Usage: ./bain.sh [-h] [-b BATTERY] [-c COLOR_CHARGE] [-u COLOR_UNCHARGE] IMAGE
+
+  -h, --help                        print this message and exits
+  -b, --battery BATTERY             the battery of the computer, if not set the programm will try to find it automatically
+  -c, --charge COLOR_CHARGE         color for charge battery, defualt #5BC236
+  -u, --uncharge COLOR_UNCHARGE     color for uncharge battery, defualt #BF131C
+  IMAGE                             the image to use for the background
+EOF
+		exit
+}
+
+file=
+COLOR_CHARGE='#5BC236'
+COLOR_UNCHARGE='#BF131C'
+
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+		-h|--help) cat_help; shift;;
+		-b|--battery) battery_path="$2"; shift 2;;
+		-c|--charge) COLOR_CHARGE="$2"; shift 2;;
+		-u|--uncharge) COLOR_UNCHARGE="$2"; shift 2;;
+		*) file="$1"; shift 1;;
+	esac
+done
+
+[ -z "$battery_path" ] && battery_path=$(find_battery_path)
 last_changed=`< $battery_path/capacity`
 create_and_set $file $last_changed
 
